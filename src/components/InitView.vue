812419395f2e333b1b3747079ffff4e48394d3f1
Felix Martin <fmartin@bigml.com>
2023-04-17 10:50:14 +0200

    fix plug animation

--- src/components/InitView.vue
+++ src/components/InitView.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" align="center" class="px-10">
       <v-img :src="logo" max-width="700" class="logo-rounded mb-5" @load="onIconLoad" />
       <span class="font-weight-light text-secondary text-body-2 pl-5" :class="{ mobile: !$vuetify.display.mdAndUp }" v-if="iconLoaded">
-        DESCONECTA<span class="points ml-2">·..</span><v-icon class="mb-3">mdi-power-plug mdi-rotate-45</v-icon><br />LAS NAVAS DE LA CONCEPCIÓN, SEVILLA
+        DESCONECTA<span class="points ml-2">..</span><v-icon class="mb-2">mdi-power-plug mdi-rotate-45</v-icon><br />LAS NAVAS DE LA CONCEPCIÓN, SEVILLA
       </span>
       <div class="d-flex align-center justify-center fill-height mt-10" v-if="imgLoading">
         <v-progress-circular color="secondary" indeterminate :width="4" :size="25" />
@@ -47,7 +47,7 @@ export default {
 #app .v-row span.points {
   font-weight: bold;
   font-size: 1.5em;
-  margin-right: -2px;
+  margin-right: -0.25%;
 }
 @keyframes fade-in {
   0% {
